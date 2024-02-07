97755d396a45316ed01e81b6ef6d2bf63ea2c56e
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2024-02-06 20:07:42 -0800

    adding postprocessing to sun

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,26 +1,48 @@
 import { useFrame } from '@react-three/fiber'
 import { Sparkles, Sky, OrbitControls } from '@react-three/drei'
 import { useRef } from 'react'
+import { useControls, folder } from 'leva'
+import { EffectComposer, ToneMapping, Bloom } from '@react-three/postprocessing'
+import { Perf } from 'r3f-perf'
+import { BlendFunction } from 'postprocessing'
 
 export default function Experience() {
-  const cube = useRef()
+  const sun = useRef()
+  console.log(sun.current)
+
   useFrame((state, delta) => {
-    cube.current.rotation.y += delta * 0.2
+    sun.current.rotation.y += delta * 0.2
   })
+  const { bloomThreshold, bloomIntensity, bloomRadius } = useControls('Effects', {
+    'Sun Glow': folder(
+      {
+        bloomThreshold: { value: 0.6, min: 0, max: 2, step: 0.1 },
+        bloomIntensity: { value: 0.3, min: 0, max: 3, step: 0.1 },
+        bloomRadius: { value: 0.9, min: 0, max: 2, step: 0.1 },
+        luminanceSmoothing: { value: 0.5, min: 0, max: 1, step: 0.1 },
+      },
+      { collapsed: false },
+    ),
+  })
+
   return (
     <>
-      <Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} />
+      <color args={['#000000']} attach="background" />
+      <EffectComposer disableNormalPass>
+        <Bloom
+          luminanceThreshold={bloomThreshold}
+          intensity={bloomIntensity}
+          radius={bloomRadius}
+          // luminanceSmoothing={luminanceSmoothing}
+        />
+        <ToneMapping />
+      </EffectComposer>
       <OrbitControls />
       <directionalLight position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={0.5} />
-      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
-        <planeGeometry />
-        <meshStandardMaterial color="green" />
-      </mesh>
-
-      <mesh position-x={1} ref={cube}>
-        <boxGeometry />
-        <meshStandardMaterial color="hotpink" />
+      <mesh ref={sun}>
+        <sphereGeometry />
+        <meshStandardMaterial emissive="orange" emissiveIntensity={2} toneMapped={false} />
       </mesh>
       <Sparkles color="purple" speed={0.2} scale={3} count={100} opacity={1} />
     </>
